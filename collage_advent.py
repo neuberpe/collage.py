99f12359eea0d12e472b409f97684b9faac68bb8
neuberpe <36083955+neuberpe@users.noreply.github.com>
2024-01-04 15:59:15 +0100

    Add files via upload

---
+++ collage_advent.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageEnhance
+size = 128
+loc=[]
+grid=[]
+new = Image.new("RGBA", (5*size,5*size))
+for i in range(25):				#25 images
+	loc.append(str(i+1)+".png")
+for i in range(5):				#5 x 5 matrix
+	for j in range(5):
+		grid.append(size*i)
+		grid.append(size*j)
+
+for k in range(25):	
+	img = Image.open(loc[k])
+	new.paste(img, (grid[2*k],grid[2*k+1]))
+
+new.show()
+new.save("collage.png")
